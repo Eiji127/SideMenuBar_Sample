30cfc8c05b2c5e0b3e9d129efeb7e0b126d628a0
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-02-20 00:18:09 +0900

    Initial Commit

---
+++ SideMenuBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuBar
+//
+//  Created by 白数叡司 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
