6794119850b03898ac69b76ea8612e000e9ef961
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-02-20 19:57:08 +0900

    first commit

---
+++ SideMenuBar/Controller/HomeController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeController.swift
+//  SideMenuBar
+//
+//  Created by 白数叡司 on 2021/02/20.
+//
+
+import Foundation
---
+++ SideMenuBar/Controller/SetttingsController.swift
@@ -0,0 +1,8 @@
+//
+//  SetttingsController.swift
+//  SideMenuBar
+//
+//  Created by 白数叡司 on 2021/02/20.
+//
+
+import Foundation
